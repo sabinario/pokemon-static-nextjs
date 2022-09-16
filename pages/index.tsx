ba3d825699b18cee2e2116224ef0979aee358653
Sabino Fernandez <sabinofernandezr@gmail.com>
2022-09-16 14:57:36 -0500

    adding styles library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
 
+import { Button } from '@nextui-org/react';
+
 const HomePage: NextPage = () => {
 	return (
 		<>
-			<h1>Hello world!</h1>
+			<Button color='gradient'>Hello world!</Button>
 		</>
 	);
 };
